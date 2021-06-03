ceea453d93189ed5293466e0d6cb8eac6e9c31b4
Jordan Leich <jordanleich@gmail.com>
2021-06-03 02:10:35 -0400

    Added time module for better formatting

--- contribution.py
+++ contribution.py
@@ -1,9 +1,11 @@
 import webbrowser
+import time
 
 def start():
     print("Opening all current contributors of this project...")
+    time.sleep(1.5)
     webbrowser.open_new("https://github.com/JordanLeich/Ultimate-Calculator/graphs/contributors")
+    time.sleep(2)
+    quit()
 
 start()
-
-
