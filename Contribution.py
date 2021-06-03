1f9ca2939d3ba0e62f45d629c45fd19e6264ea55
Jordan Leich <jordanleich@gmail.com>
2021-06-03 01:26:47 -0400

    Create Contribution.py

---
+++ Contribution.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+def start():
+    print("Opening all current contributors of this project...")
+    webbrowser.open_new("https://github.com/JordanLeich/Ultimate-Calculator/graphs/contributors")
+
+start()
+
+
