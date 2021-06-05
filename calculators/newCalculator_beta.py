72fc349b0fb366759759cfeeb08ba37d61276902
bot-coder05 <john.kenneth2525@yahoo.com>
2021-06-05 21:01:45 +0400

    New Calculator (Beta)

--- calculators/newCalculator_beta.py
+++ calculators/newCalculator_beta.py
@@ -2,11 +2,6 @@ import time
 import colors
 import restart
 
-# Variables
-operator = float
-result = float
-number1 = float
-number2 = float
 
 def calculator(data):
     total = 0
@@ -14,20 +9,36 @@ def calculator(data):
         if dta == "+":
             total += int(data[ind + 1])
             data.pop(ind + 1)
+
+        elif dta == "-":
+            total -= int(data[ind + 1])
+            data.pop(ind + 1)
+
+        elif dta == "/":
+            total /= int(data[ind + 1])
+            data.pop(ind + 1)
+
+        elif dta == "*":
+            total *= int(data[ind + 1])
+            data.pop(ind + 1)
+
+        elif dta == "**":
+            total **= int(data[ind + 1])
+            data.pop(ind + 1)
+
         elif dta == "=":
             break
+
         else:
             total += int(dta)
+
     return total
 
 
 # Entire operation
 def start():
-    global operator, result, number1, number2
-
-    # Keep asking the user until they enter a valid input
+    calculation = []
     while True:
-        calculation = []
         number = input("Your Number: ")
         operator = input("Your Operator: ")
         calculation.append(number)
@@ -37,13 +48,11 @@ def start():
             break
         print(" ".join(calculation))
 
-    # it's kinda annoying to restart after, so I added continue option
-    # change the variable names because I'm not really good with naming variables
-    input_1 = str(input("Would you like to make another arithmetic calculation (yes / no): "))
-
-    if input_1.lower() == "yes" or input_1.lower() == 'y':
+    # Better Variable Names :)
+    continue_opt = input("Would you like to make another arithmetic calculation (yes / no): ")
+    if continue_opt.lower() in ['y', 'yes']:
         start()
-    elif input_1.lower() == "no" or input_1.lower() == 'n':
+    elif continue_opt.lower() in ['n', 'no']:
         restart.restart()
     else:
         print(colors.red + 'User input error found...')
