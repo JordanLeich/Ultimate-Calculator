fea1564307fb991290fcbf5cdc1cb01a09e69cab
BadrBelkadi <Badrdinbelkadi@gmail.com>
2021-06-06 20:46:20 +0100

    Deleted temp.ui file and minor changes on some functions

--- calculators/randomization.py
+++ calculators/randomization.py
@@ -9,7 +9,8 @@ def random_number():
     end_number = int(input('Pick a ending number: '))
     print()
     number = random.randint(start_number, end_number)
-    print(colors.green, 'Your random generated number:', number, '\n', colors.reset)
+    print(colors.green, 'Your random generated number:',
+          number, '\n', colors.reset)
     restart.restart()
 
 
@@ -17,8 +18,7 @@ def heads_or_tails():
     choice = str(input('Pick heads or tails: '))
     print()
 
-    if choice.lower() != 'heads' and choice.lower() != 'h' and choice.lower() != 't' and choice.lower() != 'tails' \
-            and choice.lower() != 'tail' and choice.lower() != 'head':
+    if choice.lower() not in ['heads', 'h', 't', 'tails', 'tail', 'head']:
         print(colors.red, 'User input error found! Restarting heads or tails...')
         time.sleep(2)
         heads_or_tails()
