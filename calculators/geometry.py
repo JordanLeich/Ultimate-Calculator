b49be51dd04ee9507e937b58cc9d043313a74179
Jordan Leich <jordanleich@gmail.com>
2021-06-16 12:05:05 -0400

    added finding volume options for 3 shapes

--- calculators/geometry.py
+++ calculators/geometry.py
@@ -8,6 +8,7 @@ def start():
     choice = int(input('''(1) Find Area
 (2) Find Circumference
 (3) Find Perimeter
+(4) Find Volume
 
 Which Geometry calculator would you like to use: '''))
     print()
@@ -50,6 +51,40 @@ Which Shape would you like to solve the area for: '''))
         perimeter = a + b + c
         print(colors.green, "Perimeter:", perimeter, colors.reset, '\n')
         restart.restart()
+    elif choice == 4:
+        choice = int(input('''(1) Volume of a Cube
+(2) Volume of a Right Rectangular Prism
+(3) Volume of a Prism or Cylinder
+
+Which Shape would you like to solve the volume for: '''))
+        print()
+
+        if choice == 1:
+            side = float(input('Length of the Side: '))
+            print()
+            volume = side * side * side
+            print(colors.green, "Volume:", volume, colors.reset, '\n')
+            restart.restart()
+        elif choice == 2:
+            length = float(input('Length: '))
+            width = float(input('Width: '))
+            height = float(input('Height: '))
+            print()
+            volume = length * width * height
+            print(colors.green, "Volume:", volume, colors.reset, '\n')
+            restart.restart()
+        elif choice == 3:
+            area = float(input('Area / Base / Radius: '))
+            height = float(input('Height: '))
+            print()
+            volume = math.pi * area * area * height
+            print(colors.green, "Volume:", volume, colors.reset, '\n')
+            restart.restart()
+        else:
+            print(colors.red + "User input error found...\n", colors.reset)
+            time.sleep(2)
+            start()
+
     else:
         print(colors.red + "User input error found...\n", colors.reset)
         time.sleep(2)
