c3e9b2dcff17ed01c9f2b00a2b1402a5e50b3043
PeanutbutterWarrior <50717143+PeanutbutterWarrior@users.noreply.github.com>
2021-06-25 22:57:22 +0100

    Update currency_api.py

--- modules/currency_api.py
+++ modules/currency_api.py
@@ -3,60 +3,77 @@ Using currencyconverterapi.com API to get some
 basic currency-conversion pairings in real-time.
 MAX 100 requests/hr.
 """
-from requests import get
+import requests
 import json
 import os
 
 
-def validate_key(key):
-    url = f'https://free.currconv.com/api/v7/currencies?apiKey={key}'
-    data = get(url)
-    if data.status_code == 200:
-        save_key(key)
-        return True
+def get_api_key():
+    """
+    Returns a valid api key
+    Loads from key.json if possible
+    Prompting the user otherwise
+    """
+    if os.path.exists('../key.json'):
+        try:
+            with open('../key.json', 'r') as key_file:
+                key = json.load(key_file)
+            key = key['key']
+        except (IndexError, json.decoder.JSONDecodeError):
+            print('key.json is corrupted')
+            key = _create_new_api_key()
+            _save_key(key)
+        if not _validate_key(key):
+            print('Invalid key found')
+            key = _create_new_api_key()
+            _save_key(key)
     else:
-        return False
+        key = _create_new_api_key()
+        _save_key(key)
+    return key
 
 
-def create_key_json():
-    with open("key.json", 'w'):
-        pass
+def _create_new_api_key():
+    print("""To use the currency conversion an API key is required.
+Visit www.currencyconverterapi.com and follow the steps to get an API key.""")
+    while True:
+        key = input("API Key: ")
+        if _validate_key(key):
+            break
+        else:
+            print('Not a valid key')
+    return key
 
 
-def save_key(key):
+def _validate_key(key):
+    url = f'https://free.currconv.com/api/v7/currencies?apiKey={key}'
+    data = requests.get(url)
+    if data.status_code == 200:
+        return True
+    return False
+
+
+def _save_key(key):
     data = {'key': key}
-    with open("../key.json", "w") as outfile:
+    with open("key.json", "w") as outfile:
         json.dump(data, outfile)
 
 
-def get_currency_pairs():
+def get_currencies(api_key):
     """
-    List of all possible currency pairings
-    available on this API.
-
-    THIS STILL NEEDS WORK TO PUT TOGETHER
-    EVERY POSSIBLE CURRENCY COMBINATION.
+    List of all currencies available on this API.
     """
-    api_key = os.environ.get('API_KEY')
     url = 'https://free.currconv.com/api/v7/currencies?apiKey={}'.format(api_key)
-    data = get(url)
-    currencies = [currency for currency in data.json()['results']]
-    pairs = []
-
-    for ind, currency in enumerate(currencies):
-        if ind != 0:
-            pairs.append('ALL_{}'.format(currency))
-
-    return pairs
+    data = requests.get(url)
+    return list(data.json()['results'].values())
 
 
-def get_currency(req_pair, api_key):
-    url = "https://free.currconv.com/api/v7/convert?q={}&compact=ultra&apiKey={}".format(req_pair, api_key)
-    data = get(url)
-    return data.json()[req_pair]
+def get_currency(currency_1, currency_2, api_key):
+    """
+    Gets the exchange rate between two currencies
+    """
+    currency_pair = currency_1 + '-' + currency_2
+    url = "https://free.currconv.com/api/v7/convert?q={}&compact=ultra&apiKey={}".format(currency_pair, api_key)
+    data = requests.get(url)
+    return data.json()[currency_pair]
 
-
-if __name__ == '__main__':
-    urls = "https://api.exchangerate.host/latest?base=USD&symbols=EUR"
-    panda = get(urls)
-    print(panda.json())
